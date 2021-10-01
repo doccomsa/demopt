ae6eb23ffd010d1900ee35a86a371530ebeaaac2
Administrator <Administrator@704-02>
2021-10-01 17:48:19 +0900

    2번째 수정

--- demoPT/src/ch01/Hello.java
+++ demoPT/src/ch01/Hello.java
@@ -2,6 +2,7 @@ package ch01;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("git을 공부하자.");
+		System.out.println("git을 공부하자1.");
+		System.out.println("git을 공부하자2.");
 	}
 }
